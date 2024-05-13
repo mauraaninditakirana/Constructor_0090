a43df79b2c3d1e4b232728fcbe338cdcaeead095
mauraaninditakirana <kiranamaura351@gmail.com>
2024-05-13 15:53:33 +0700

    membuat program constructor dan menjalankannya

--- constructor3.cpp
+++ constructor3.cpp
@@ -35,6 +35,20 @@ Mahasiswa::Mahasiswa(int iNim, string iNama)
     nim = iNim;
     nama = iNama;
 };
+int main()
+{
+ Mahasiswa mhs1;
+ Mahasiswa mhs2(90);
+ Mahasiswa mhs3("Maura");
+ Mahasiswa mhs4(90, "Maura"); 
 
+ mhs1.cetak();
+ mhs2.cetak();
+ mhs3.cetak();
+ mhs4.cetak();
+
+return 0;
+
+}
 
 
