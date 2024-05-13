ec8d6959195b00a38841db7c6377809fd1253edf
mauraaninditakirana <kiranamaura351@gmail.com>
2024-05-13 17:40:14 +0700

    membuat class buku dengan string judul dan public untuk set dan get judul bukunya

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
-#include <string>
 using namespace std;
+class buku
+{
+    string judul;
+
+public:
+    buku setJudul(string judul)
+    {
+        this->judul = judul;
+        return *this;
+    }
+    string getJudul()
+    {
+        return this->judul;
+    }
+} bukunya;
