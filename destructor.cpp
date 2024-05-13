28e79559d59cf7b52fd716749c21541fa525d706
mauraaninditakirana <kiranamaura351@gmail.com>
2024-05-13 17:31:59 +0700

    membuat prosedur void angka untuk menampilkan isi data

--- destructor.cpp
+++ destructor.cpp
@@ -23,3 +23,9 @@ angka::~angka(){
     cout << "Alamat Array Sudah Dilepaskan " << endl;
 
 }
+void angka::isiData(){
+    for(int i=1;i<=panjang;i++){
+        cout << i << " = "; cin >>arr[i];
+    }
+    cout << endl;
+}
