b9c6666d28ecf63817a9f40309a2293ae4b6b41d
mauraaninditakirana <kiranamaura351@gmail.com>
2024-05-13 15:08:01 +0700

    membuat nilai awal dengan nim=100

--- constructor2.cpp
+++ constructor2.cpp
@@ -16,5 +16,10 @@ class mahasiswa
         static void setNim(int pNim) {nim = pNim; }
         static int getNim() {return nim; }
 
-    
-}
+         mahasiswa(string pnama) :nama (pnama)
+        {
+           setID(); 
+        }
+};
+
+int mahasiswa :: nim = 100;
