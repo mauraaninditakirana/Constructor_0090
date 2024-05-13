b05d6f6d766cd43d7c7ffecb9fa7076704820e28
mauraaninditakirana <kiranamaura351@gmail.com>
2024-05-13 15:28:26 +0700

    membuat class mahasiswa public

---
+++ constructor3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa
+{
+    public:
+        int nim;
+        string nama;
+        Mahasiswa()
+        {
+            nim = 0;
+            nama = "";
+        };
+        Mahasiswa(int iNim)
+        {
+            nim = iNim;
+        };
+};
