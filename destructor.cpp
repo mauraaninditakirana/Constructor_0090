c52befd217b843dc7b3a8b269b55e8bf63758f76
mauraaninditakirana <kiranamaura351@gmail.com>
2024-05-13 17:29:49 +0700

    membuat prosedur angka constructor dan destructor

--- destructor.cpp
+++ destructor.cpp
@@ -11,3 +11,15 @@ public:
     void cetakData();
     void isiData();
 };
+angka::angka(int i){
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+angka::~angka(){
+    cout << endl;
+    cetakData();
+    delete[]arr;
+    cout << "Alamat Array Sudah Dilepaskan " << endl;
+
+}
